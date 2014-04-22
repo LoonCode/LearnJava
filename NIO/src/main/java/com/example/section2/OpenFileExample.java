5a5a91ef471ebcf0aae6209180045cd62101c5e5
Loon <test>
2014-04-22 13:13:35 +0800

    io

--- NIO/src/main/java/com/example/section2/OpenFileExample.java
+++ NIO/src/main/java/com/example/section2/OpenFileExample.java
@@ -12,12 +12,22 @@ import java.net.URL;
 
 public class OpenFileExample {
 
-
+    /**
+     * @param url 文件Url
+     * @throws URISyntaxException
+     * @throws IOException
+     */
     public static void openFileForURL(URL url) throws URISyntaxException, IOException {
         File file = new File(url.toURI());
         System.out.println(Files.readFirstLine(file, Charsets.UTF_8));
     }
 
+    /**
+     *
+     * @param path 文件路径
+     * @throws URISyntaxException
+     * @throws IOException
+     */
     public static void openFileForPath(String path) throws URISyntaxException, IOException {
         File file = new File(path);
         System.out.println(Files.readFirstLine(file, Charsets.UTF_8));

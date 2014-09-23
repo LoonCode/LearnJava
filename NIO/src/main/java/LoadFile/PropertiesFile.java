4ba7485aa986cf4dad81082a8aeac0871db7780c
Loon <ppgl1988@gmail.com>
2014-09-23 13:12:03 +0800

    quartz

---
+++ NIO/src/main/java/LoadFile/PropertiesFile.java
@@ -0,0 +1,36 @@
+package LoadFile;
+
+import com.google.common.collect.Maps;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * Created by Loon on 2014/9/23.
+ */
+public class PropertiesFile {
+
+
+    public static void main(String[] args) throws IOException {
+        getPropertiesFile();
+    }
+
+    /**
+     * 加载Properties文件
+     *
+     * @return
+     * @throws IOException
+     */
+    private static String getPropertiesFile() throws IOException {
+
+        Resource resource = new ClassPathResource("config.properties");
+        Properties properties = PropertiesLoaderUtils.loadProperties(resource);
+        ;
+        Map<String, String> variableMap = Maps.fromProperties(properties);
+        return variableMap.get("test");
+    }
+}

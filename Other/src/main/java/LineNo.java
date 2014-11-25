171ea97545a9e87fb76f26552602233e97b1a15e
Loon <ppgl1988@gmail.com>
2014-11-25 17:20:01 +0800

    新增其他模块，一些无法归类的测试代码

---
+++ Other/src/main/java/LineNo.java
@@ -0,0 +1,20 @@
+/**
+ * 获取文件名称，行号
+ *
+ * @link http://www.coolside.cn/?p=59
+ * Created by Loon on 2014/11/25.
+ */
+public class LineNo {
+
+    public static int getLineNumber() {
+        return Thread.currentThread().getStackTrace()[2].getLineNumber();
+    }
+
+    public static String getFileName() {
+        return Thread.currentThread().getStackTrace()[2].getFileName();
+    }
+
+    public static void main(String args[]) {
+        System.out.println("[" + getFileName() + "：" + getLineNumber() + "]" + "Hello World!");
+    }
+}

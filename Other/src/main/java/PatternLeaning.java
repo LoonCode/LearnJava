9223a540855d5699b34c359d51fdc9cf6eacab12
Loon <ppgl1988@gmail.com>
2015-11-10 14:47:47 +0800

    正则表达式

---
+++ Other/src/main/java/PatternLeaning.java
@@ -0,0 +1,43 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Loon on 2015/11/10.
+ */
+public class PatternLeaning {
+
+    public static void main(String[] args) {
+
+        String aa ="\\d";
+
+        Pattern pattern = Pattern.compile(aa);
+
+        System.out.println(replaceMatcherString("aadsf111d111s",pattern)) ;
+        System.out.println(getMatcherString("aadsf111ds1",pattern));
+    }
+
+    private static String getMatcherString(String str, Pattern pattern) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        Matcher matcher = pattern.matcher(str);
+
+        while(matcher.find()){
+            stringBuilder.append(matcher.group());
+        }
+
+        return stringBuilder.toString();
+    }
+
+    private static String replaceMatcherString(String str, Pattern pattern) {
+
+        StringBuffer stringBuffer = new StringBuffer();
+        Matcher matcher = pattern.matcher(str);
+
+        while(matcher.find()){
+            matcher.appendReplacement(stringBuffer,"");
+        }
+        matcher.appendTail(stringBuffer);
+
+        return stringBuffer.toString();
+    }
+}
